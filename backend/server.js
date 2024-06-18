b66fafecce730aac91c40d4fe22b4bbe7fd9307f
KSHITIZ RAI <kshitizroy836@gmail.com>
2024-06-19 00:36:25 +0530

    message model and schema

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import authRoutes from "./Routes/auth.route.js"
+import messageRoutes from "./Routes/message.route.js"
 import express from "express"
 import dotenv from "dotenv"
 import connectTOMongoDB from "./DB/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); 
-app.use("/api/auth",authRoutes)
+app.use(cookieParser());
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
 
 app.get("/",(req,res) => {
     //root route
